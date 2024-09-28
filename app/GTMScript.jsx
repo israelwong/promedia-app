9b691c6c26688fe8c57e90b98f99de1e757ae9be
Israel Wong <israelwong@iMac-de-Israel.local>
2024-09-28 14:21:05 -0600

    GTMScript

---
+++ app/GTMScript.jsx
@@ -0,0 +1,21 @@
+"use client";
+import { useEffect } from "react";
+
+const GTMScript = () => {
+  useEffect(() => {
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-M9ZT7HQ9");
+  }, []);
+
+  return null;
+};
+
+export default GTMScript;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,9 @@ import Footer from "@/app/ui/main/Footer";
 import { ThemeProvider } from "@/app/ui/theme-provider";
 import Script from 'next/script';
 import "@/app/globals.css";
+import GTMScript from './GTMScript'; // Ajusta la ruta según sea necesario
 
-// import { GoogleTagManager } from '@next/third-parties/google'
 
-// import TagManager from '@sooro-io/react-gtm-module'
-// const tagManagerArgs = {gtmId: 'GTM-M9ZT7HQ9'}
-// TagManager.initialize(tagManagerArgs)
-
-// end changes
 
 export const metadata: Metadata = {
   title: {
@@ -41,27 +36,12 @@ export default function RootLayout({
 
         <Script src="https://kit.fontawesome.com/74d1405387.js"/>
 
-        {(process.env.NODE_ENV === 'production') &&
-        <script dangerouslySetInnerHTML={{
-          __html: `(function (w, d, s, l, i) {
-                  w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-                  var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-                  j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-M9ZT7HQ9' + i + dl; f.parentNode.insertBefore(j, f);
-              })(window, document, 'script', 'dataLayer', 'GTM-M9ZT7HQ9');`
-        }}></script>
-        }
 
       </head>
 
       {/* <GoogleTagManager gtmId="GTM-M9ZT7HQ9" /> */}
       <body className={`antialiased bg-black`}>
-
-      {(process.env.NODE_ENV === 'production') &&
-        <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9ZT7HQ9" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-        }}></noscript>
-      }
-
+        <GTMScript />
         <ThemeProvider>
           <Navbar />
           {children}
